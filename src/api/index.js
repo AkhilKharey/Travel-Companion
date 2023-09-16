a348d4cc04bc794d401ca3e76c14d033317ac8a6
akhilkhare <akhilkharey9340@gmail.com>
2023-09-17 00:49:38 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': '36cbfe3f06msh25df00b2e7940e9p1763dbjsnf7562132bb40',
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
